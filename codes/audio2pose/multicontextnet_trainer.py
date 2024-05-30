270c7c51cb54769a5950f83a5fc7aeabd6c4405f
Neil Smith <ngsmith@ucsd.edu>
2024-05-30 01:40:07 -0700

    added and separated vertex and blendshape smooth loss

--- codes/audio2pose/multicontextnet_trainer.py
+++ codes/audio2pose/multicontextnet_trainer.py
@@ -77,7 +77,8 @@ class Trainer():
         self.model.eval()
         val_target_loss_st = []
         #val_expressive_loss_st = []
-        val_smooth_loss_st = []
+        val_vt_smooth_loss_st = []
+        val_bs_smooth_loss_st = []
         val_cnt = 0
 
         val_loader = torch.utils.data.DataLoader(
@@ -89,37 +90,42 @@ class Trainer():
         
         for _, data in enumerate(val_loader):
             in_audio = data['audio']
-            facial = data['facial']
+            bs_facial = data['facial']
             in_id = data["id"]
             in_word = data["word"]
             in_emo = data["emo"]
 
             in_audio = in_audio.cuda()
-            facial = facial.cuda()
+            bs_facial = bs_facial.cuda()
+            vt_facial = bs_facial @ self.to_flame
             in_id = in_id.cuda()
             in_word = in_word.cuda()
             in_emo = in_emo.cuda()
 
             pre_frames = 4
-            in_pre_face = facial.new_zeros((facial.shape[0], facial.shape[1], facial.shape[2] + 1)).cuda()
-            in_pre_face[:, 0:pre_frames, :-1] = facial[:, 0:pre_frames]
+            in_pre_face = bs_facial.new_zeros((bs_facial.shape[0], bs_facial.shape[1], bs_facial.shape[2] + 1)).cuda()
+            in_pre_face[:, 0:pre_frames, :-1] = bs_facial[:, 0:pre_frames]
             in_pre_face[:, 0:pre_frames, -1] = 1 
 
-            out_face = self.model(in_pre_face,in_audio=in_audio,in_text=in_word, in_id=in_id, in_emo=in_emo)
-            target_loss = self.target_loss_function(out_face@self.to_flame, facial@self.to_flame) + self.target_loss_function(out_face[:,:,6:14], facial[:,:,6:14])
+            bs_out_face = self.model(in_pre_face,in_audio=in_audio,in_text=in_word, in_id=in_id, in_emo=in_emo)
+            vt_out_face = bs_out_face @ self.to_flame
+            target_loss = self.target_loss_function(vt_out_face, vt_facial) + self.target_loss_function(bs_out_face[:,:,6:14], bs_facial[:,:,6:14]) # to account for eye movement
             #expressive_loss = self.expressive_loss_function(out_face, facial)
-            smooth_loss = 1 - self.smooth_loss_function(out_face[:,:-1,:], out_face[:,1:,:]).mean()
+            vt_smooth_loss = 1 - self.smooth_loss_function(vt_out_face[:,:-1,:], vt_out_face[:,1:,:]).mean()
+            bs_smooth_loss = 1 - self.smooth_loss_function(bs_out_face[:,:-1,:], bs_out_face[:,1:,:]).mean()
 
             val_target_loss_st.append(target_loss.item())
             #val_expressive_loss_st.append(expressive_loss.item())
-            val_smooth_loss_st.append(smooth_loss.item())                        
+            val_vt_smooth_loss_st.append(vt_smooth_loss.item())      
+            val_bs_smooth_loss_st.append(bs_smooth_loss.item())                    
             
             val_cnt += 1
             if val_cnt >= self.val_size:
                 break
         return {
 			"target_loss": float(np.average(val_target_loss_st)),
-			"smooth_loss": float(np.average(val_smooth_loss_st)),
+			"vt_smooth_loss": float(np.average(val_vt_smooth_loss_st)),
+            "bs_smooth_loss": float(np.average(val_bs_smooth_loss_st)),
 			#"expressive_loss": float(np.average(val_expressive_loss_st)),
 		}
 
@@ -128,46 +134,51 @@ class Trainer():
             for it, data in enumerate(self.train_loader):
                 self.model.train()
                 in_audio = data['audio']
-                facial = data['facial']
+                bs_facial = data['facial']
                 in_id = data["id"]
                 in_word = data["word"]
                 in_emo = data["emo"]
 
                 in_audio = in_audio.cuda()
-                facial = facial.cuda()
+                bs_facial = bs_facial.cuda()
+                vt_facial = bs_facial@self.to_flame
                 in_id = in_id.cuda()
                 in_word = in_word.cuda()
                 in_emo = in_emo.cuda()
 
                 pre_frames = 4
-                in_pre_face = facial.new_zeros((facial.shape[0], facial.shape[1], facial.shape[2] + 1)).cuda()
-                in_pre_face[:, 0:pre_frames, :-1] = facial[:, 0:pre_frames]
+                in_pre_face = bs_facial.new_zeros((bs_facial.shape[0], bs_facial.shape[1], bs_facial.shape[2] + 1)).cuda()
+                in_pre_face[:, 0:pre_frames, :-1] = bs_facial[:, 0:pre_frames]
                 in_pre_face[:, 0:pre_frames, -1] = 1 
                 
                 self.optimizer.zero_grad()
-                out_face = self.model(in_pre_face,in_audio=in_audio,in_text=in_word, in_id=in_id, in_emo=in_emo)
-                target_loss = self.target_loss_function(out_face@self.to_flame, facial@self.to_flame) + self.target_loss_function(out_face[:,:,6:14], facial[:,:,6:14]) # to account for eye movement
+                bs_out_face = self.model(in_pre_face,in_audio=in_audio,in_text=in_word, in_id=in_id, in_emo=in_emo)
+                vt_out_face = bs_out_face@self.to_flame
+
+                target_loss = self.target_loss_function(vt_out_face, vt_facial) + self.target_loss_function(bs_out_face[:,:,6:14], bs_facial[:,:,6:14]) # to account for eye movement
                 #expressive_loss = self.expressive_loss_function(out_face, facial)
-                smooth_loss = 1 - self.smooth_loss_function(out_face[:,:-1,:], out_face[:,1:,:]).mean()
-                loss = self.target_weight * target_loss  + self.smooth_weight * smooth_loss# + expressive_weight * expressive_loss
+                vt_smooth_loss = 1 - self.smooth_loss_function(vt_out_face[:,:-1,:], vt_out_face[:,1:,:]).mean()
+                bs_smooth_loss = 1 - self.smooth_loss_function(bs_out_face[:,:-1,:], bs_out_face[:,1:,:]).mean()
+                loss = self.target_weight * target_loss  + self.smooth_weight * vt_smooth_loss + self.smooth_weight * bs_smooth_loss# + expressive_weight * expressive_loss
                 loss.backward()
                 self.optimizer.step()
 
                 if it % self.log_period == 0:
                     train_metrics = {
                         "target_loss": float(target_loss.item()),
-                        "smooth_loss": float(smooth_loss.item()),
+                        "vt_smooth_loss": float(vt_smooth_loss.item()),
+                        "bs_smooth_loss": float(bs_smooth_loss.item())
                         #"expressive_loss": float(expressive_loss.item()),
                     }
                     train_metrics.update(self.common_metrics())
                     self.logger.log(train_metrics, 'train')
-                    print(f'[{self._ep_idx}][{it}/{len(self.train_loader)}]: [train] [target loss]: {train_metrics["target_loss"]} [smooth loss]: {train_metrics["smooth_loss"]}')
+                    print(f'[{self._ep_idx}][{it}/{len(self.train_loader)}]: [train] [target loss]: {train_metrics["target_loss"]} [vt smooth loss]: {train_metrics["vt_smooth_loss"]}  [bs smooth loss]: {train_metrics["bs_smooth_loss"]}')
 
                 if it % self.val_period == 0:
                     val_metrics = self.val()
                     val_metrics.update(self.common_metrics())
                     self.logger.log(val_metrics,'val')
-                    print(f'[{self._ep_idx}][{it}/{len(self.train_loader)}]: [val] [target loss]: {val_metrics["target_loss"]} [smooth loss]: {val_metrics["smooth_loss"]}')
+                    print(f'[{self._ep_idx}][{it}/{len(self.train_loader)}]: [val] [target loss]: {val_metrics["target_loss"]} [vt smooth loss]: {val_metrics["vt_smooth_loss"]}  [bs smooth loss]: {val_metrics["bs_smooth_loss"]}')
 
                 self._iter += 1
             if (self._ep_idx+1) % self.save_period == 0:
